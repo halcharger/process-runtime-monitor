d089506155d658de1988ae3de71bcac4807b78b0
unknown <allenf@DATPCDI032.digiterre.com>
2013-12-06 11:30:29 +0000

    configured to use list of processes in app.config

--- process-runtime-monitor/ProcessMonitorJob.cs
+++ process-runtime-monitor/ProcessMonitorJob.cs
@@ -7,7 +7,10 @@ namespace process_runtime_monitor
     {
         public void Execute(IJobExecutionContext context)
         {
-            Console.WriteLine("running ProcessMonitorJob {0}", DateTime.Now.ToLongTimeString());
+            if (Program.ProcessesToMonitor.Count == 0)
+                Console.WriteLine("No processes to monitor: {0}", DateTime.Now.ToLongTimeString());
+            else
+                Console.WriteLine("Monitoring for processes: {0} at {1}", string.Join(", ", Program.ProcessesToMonitor), DateTime.Now.ToLongTimeString());
         }
     }
 }
--- process-runtime-monitor/Program.cs
+++ process-runtime-monitor/Program.cs
@@ -1,4 +1,7 @@
-﻿using Quartz;
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using Quartz;
 using Topshelf;
 using Topshelf.Quartz;
 
@@ -6,12 +9,30 @@ namespace process_runtime_monitor
 {
     class Program
     {
+        public static List<Process> Processes = new List<Process>();
+        public static List<string> ProcessesToMonitor = new List<string>();
+
         static void Main(string[] args)
         {
-            HostFactory.Run(c => c.ScheduleQuartzJobAsService(q => q.WithJob(() => JobBuilder.Create<ProcessMonitorJob>().Build())
-                                                                    .AddTrigger(() => TriggerBuilder.Create()
-                                                                                                    .WithSimpleSchedule(builder => builder.WithIntervalInSeconds(10).RepeatForever())
-                                                                                                    .Build())));
+            ConfigureProcessesToMonitor();
+            ConfigureProcessMonitorJob();
+        }
+
+        static void ConfigureProcessesToMonitor()
+        {
+            ProcessesToMonitor = (ConfigurationManager.AppSettings["processes-to-monitor"] ?? string.Empty).Split(',').ToList();
+        }
+
+        static void ConfigureProcessMonitorJob()
+        {
+            HostFactory.Run(c =>
+            {
+                c.ScheduleQuartzJobAsService(q => q.WithJob(() => JobBuilder.Create<ProcessMonitorJob>().Build())
+                    .AddTrigger(() => TriggerBuilder.Create()
+                        .WithSimpleSchedule(builder => builder.WithIntervalInSeconds(10).RepeatForever())
+                        .Build()));
+
+            });
         }
     }
 }
