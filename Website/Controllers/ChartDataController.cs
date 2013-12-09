dade3e325a562d10a6463c69cf505b135de404d1
unknown <allenf@DATPCDI032.digiterre.com>
2013-12-09 12:29:20 +0000

    adding the WebAPI controller to get the chart data

---
+++ Website/Controllers/ChartDataController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+using Website.Models;
+
+namespace Website.Controllers
+{
+    public class ChartDataController : ApiController
+    {
+        // GET api/<controller>
+        public ChartModel Get()
+        {
+            return new ChartModel
+            {
+                labels = new[] { "January", "February", "March", "April", "May", "June", "July" }, 
+                datasets = new []{new Dataset
+                {
+                    fillColor = "rgba(151,187,205,0.5)",
+                    strokeColor = "rgba(151,187,205,1)",
+                    data = new decimal[] {28, 48, 40, 19, 96, 27, 100}
+                } }
+            };
+        }
+    }
+}
---
+++ Website/Models/ChartModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Website.Models
+{
+    public class ChartModel
+    {
+        public string[] labels { get; set; }
+        public Dataset[] datasets { get; set; }
+    }
+
+    public class Dataset
+    {
+        public string fillColor { get; set; }
+        public string strokeColor { get; set; }
+        public decimal[] data { get; set; }
+    }
+}
